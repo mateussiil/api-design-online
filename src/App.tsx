5d7c72b0e81efe4581a527d9d5d5a0d2ce620d16
Mateus Oliveira <mateussiiloliveira@gmal.com>
2021-03-12 23:19:38 -0300

    :adhesive_bandage: gitmoji -i
    Tem que passar um objeto ao JSON.strinfiy

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,14 @@ function App() {
   const [message, setMessage] = useState("http://localhost:3336/infornota/estados");
 
   return (
-    <div style={{ overflowY: 'hidden',height: '100vh'}} className="App">
+    <div style={{ overflowY: 'hidden', height: '100vh' }} className="App">
       <Splitter >
         <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={20} minSize={10}>
           Panel 1
     </SplitterPanel>
         <SplitterPanel size={80}>
           <Splitter layout="vertical">
-            <SplitterPanel  style={{height:'10vh', flexDirection: 'row', justifyContent: 'space-between' }} className="p-d-flex p-ai-center  p-jc-between" size={15}>
+            <SplitterPanel style={{ height: '10vh', flexDirection: 'row', justifyContent: 'space-between' }} className="p-d-flex p-ai-center  p-jc-between" size={15}>
               <Dropdown optionLabel="label" optionValue="value" value={verbs} options={verbs} onChange={(e) => setVerb(e.value)} placeholder="Select a Verb Http" />
               <InputText value={url} onChange={(e) => setUrl(e.currentTarget.value)} />
               <Button label="Click" onClick={(e) => {
@@ -43,32 +43,33 @@ function App() {
                 fetch(`${url}`, { method: verb })
                   .then(response => response.text())
                   .then(texto => {
+                    console.log(texto)
                     const element = document.getElementById("custom")
                     element!.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(JSON.parse(texto), undefined, 4));
                   })
                   .catch(err => {
 
-                    setMessage(JSON.stringify({Message:err.message}))
+                    setMessage(JSON.stringify({ Message: err.message }))
                     document.getElementById("pre")
                   })
 
-                  function syntaxHighlight(json:string) {
-                    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-                    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match:any) {
-                        var cls = 'number';
-                        if (/^"/.test(match)) {
-                            if (/:$/.test(match)) {
-                                cls = 'key';
-                            } else {
-                                cls = 'string';
-                            }
-                        } else if (/true|false/.test(match)) {
-                            cls = 'boolean';
-                        } else if (/null/.test(match)) {
-                            cls = 'null';
-                        }
-                        return '<span class="' + cls + '">' + match + '</span>';
-                    });
+                function syntaxHighlight(json: string) {
+                  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match: any) {
+                    var cls = 'number';
+                    if (/^"/.test(match)) {
+                      if (/:$/.test(match)) {
+                        cls = 'key';
+                      } else {
+                        cls = 'string';
+                      }
+                    } else if (/true|false/.test(match)) {
+                      cls = 'boolean';
+                    } else if (/null/.test(match)) {
+                      cls = 'null';
+                    }
+                    return '<span class="' + cls + '">' + match + '</span>';
+                  });
                 }
               }} />
             </SplitterPanel>
@@ -78,8 +79,8 @@ function App() {
                   Panel 3
                     </SplitterPanel>
                 <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={80}>
-                  <ScrollPanel style={{color: 'green',width: '100%', height: '90vh'}} className="custom" >
-                  <div id="custom"></div>
+                  <ScrollPanel style={{ color: 'green', width: '100%', height: '90vh' }} className="custom" >
+                    <div id="custom"></div>
                   </ScrollPanel>
                 </SplitterPanel>
               </Splitter>
