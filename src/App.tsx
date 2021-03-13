03243223e7c8e9342985bd107f76d32946a82749
Mateus Oliveira <mateussiiloliveira@gmal.com>
2021-03-13 17:31:43 -0300

    Estilizando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
 
 import './App.css';
 import './styles.css';
@@ -10,75 +8,88 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import { Dropdown } from 'primereact/dropdown';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
 import { Splitter, SplitterPanel } from 'primereact/splitter';
 import { ScrollPanel } from 'primereact/scrollpanel';
 
 const verbs = [
-  { label: 'get', value: 'GET' },
-  { label: 'post', value: 'POST' },
-  { label: 'put', value: 'PUT' },
-  { label: 'delete', value: 'DELETE' },
+  { label: 'GET', value: 'GET', color: '#2980b9' },
+  { label: 'POST', value: 'POST', color: '#16a085' },
+  { label: 'PUT', value: 'PUT', color: '#e67e22' },
+  { label: 'DELETE', value: 'DELETE', color: '#c0392b' },
 ];
+
+
 
 function App() {
   const [url, setUrl] = useState("")
   const [verb, setVerb] = useState("");
   const [message, setMessage] = useState("http://localhost:3336/infornota/estados");
 
+  const verbOptionTemplate = (option: any) => {
+    return (
+      <div style={{ color: option.color, fontWeight: 'bold' }}>{option.label}</div>
+    );
+  }
+
   return (
     <div style={{ overflowY: 'hidden', height: '100vh' }} className="App">
-      <Splitter >
-        <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={20} minSize={10}>
+      <Splitter  style={{height: '100%' }} >
+        <SplitterPanel size={30} minSize={20}>
           Panel 1
-    </SplitterPanel>
-        <SplitterPanel size={80}>
-          <Splitter layout="vertical">
-            <SplitterPanel style={{ height: '10vh', flexDirection: 'row', justifyContent: 'space-between' }} className="p-d-flex p-ai-center  p-jc-between" size={15}>
-              <Dropdown optionLabel="label" optionValue="value" value={verbs} options={verbs} onChange={(e) => setVerb(e.value)} placeholder="Select a Verb Http" />
-              <InputText value={url} onChange={(e) => setUrl(e.currentTarget.value)} />
-              <Button label="Click" onClick={(e) => {
-                e.preventDefault()
-                fetch(`${url}`, { method: verb })
-                  .then(response => response.text())
-                  .then(texto => {
-                    console.log(texto)
-                    const element = document.getElementById("custom")
-                    element!.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(JSON.parse(texto), undefined, 4));
-                  })
-                  .catch(err => {
-
-                    setMessage(JSON.stringify({ Message: err.message }))
-                    document.getElementById("pre")
-                  })
-
-                function syntaxHighlight(json: string) {
-                  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-                  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match: any) {
-                    var cls = 'number';
-                    if (/^"/.test(match)) {
-                      if (/:$/.test(match)) {
-                        cls = 'key';
-                      } else {
-                        cls = 'string';
+        </SplitterPanel>
+        <SplitterPanel size={70}>
+          <Splitter gutterSize={0} layout="vertical">
+            <SplitterPanel size={80}>
+              <div style={{height:'100%',borderBottom: '1px solid black', display: 'flex', flexDirection: 'row' }}>
+                <Dropdown className="dropdown-verb" style={{ outline: 'none', height: '100%' }} itemTemplate={verbOptionTemplate} optionLabel="value" optionValue="value" value={verb} options={verbs} onChange={(e) => setVerb(e.value)} />
+                <input className="input-verb" onChange={(e) => setUrl(e.currentTarget.value)}></input>
+                {/* <InputText value={url} onChange={(e) => setUrl(e.currentTarget.value)} /> */}
+                <button className="button-verb " onClick={(e) => {
+                  e.preventDefault()
+                  console.log('ok')
+                  fetch(`${url}`, { method: verb })
+                    .then(response => response.text())
+                    .then(texto => {
+                      console.log(texto)
+                      const element = document.getElementById("custom")
+                      const pre = document.getElementsByTagName('pre')
+                      for(var i =0 ;i<pre.length;i++){
+                        element!.removeChild(pre[i])
                       }
-                    } else if (/true|false/.test(match)) {
-                      cls = 'boolean';
-                    } else if (/null/.test(match)) {
-                      cls = 'null';
-                    }
-                    return '<span class="' + cls + '">' + match + '</span>';
-                  });
-                }
-              }} />
+                      element!.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(JSON.parse(texto), undefined, 4));
+                    })
+                    .catch(err => {
+                      setMessage(JSON.stringify({ Message: err.message }))
+                      document.getElementById("pre")
+                    })
+
+                  function syntaxHighlight(json: string) {
+                    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match: any) {
+                      var cls = 'number';
+                      if (/^"/.test(match)) {
+                        if (/:$/.test(match)) {
+                          cls = 'key';
+                        } else {
+                          cls = 'string';
+                        }
+                      } else if (/true|false/.test(match)) {
+                        cls = 'boolean';
+                      } else if (/null/.test(match)) {
+                        cls = 'null';
+                      }
+                      return '<span class="' + cls + '">' + match + '</span>';
+                    });
+                  }
+                }} >Enviar</button>
+              </div>
             </SplitterPanel>
-            <SplitterPanel size={85}>
+            <SplitterPanel size={20}>
               <Splitter>
-                <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={80}>
-                  Panel 3
-                    </SplitterPanel>
-                <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={80}>
+                <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={50}>
+                  <textarea></textarea>
+                </SplitterPanel>
+                <SplitterPanel className="p-d-flex p-ai-center p-jc-center" size={50}>
                   <ScrollPanel style={{ color: 'green', width: '100%', height: '90vh' }} className="custom" >
                     <div id="custom"></div>
                   </ScrollPanel>
